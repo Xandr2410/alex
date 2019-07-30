5a452ad5c3b2bd23e3d45ac0e46487378e8adcfa
Alex <alejandromsc10@gmail.com>
2019-07-30 16:07:09 +0300

    sec commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+app.get('/test', function(req, res){
+  res.render('test');
+});
+
+
+
+app.listen(3000);
